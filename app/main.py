eea3e30dfd1a6c9a2b3fcb203a9facb53c25571f
Windsor, Jeff <jeffrey.windsor@cj.com>
2024-12-17 12:29:40 -0800

    Concurrent Requests

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 import socket  # noqa: F401
+import threading
 from .protocol import decode_request, encode_response
 
 
 def handle_client(client):
-    request_bytes = client.recv(2048)
-    response_bytes = encode_response(decode_request(request_bytes))
-    client.sendall(response_bytes)
+    while True:
+        request_bytes = client.recv(2048)
+        response_bytes = encode_response(decode_request(request_bytes))
+        client.send(response_bytes)
 
 
 def main():
@@ -15,8 +17,8 @@ def main():
 
     while True:
         client, _ = server.accept()
-        while True:
-            handle_client(client)
+        t = threading.Thread(target=handle_client, args=(client,))
+        t.start()
 
 
 if __name__ == "__main__":
