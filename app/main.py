841a025769919e42b944af0b5eb97bd76eba2c33
Jeff Windsor <jeff.windsor@gmail.com>
2024-12-16 16:28:29 -0800

    Found 1 issue

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def handle_request(req_str: str) -> str:
 
 
 def handle_client(client):
-    req_str = client.rev(1024)
+    req_str = client.recv(1024)
     print(req_str)
     resp_str = handle_request(req_str)
     print(resp_str)
