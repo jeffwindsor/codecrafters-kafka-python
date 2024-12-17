f8e99d3e18fe3d01a86d3e809953edd58dc06977
Windsor, Jeff <jeffrey.windsor@cj.com>
2024-12-17 12:24:14 -0800

    oops

--- app/main.py
+++ app/main.py
@@ -3,6 +3,9 @@ from .protocol import decode_request, encode_response
 
 
 def handle_client(client):
+    request_bytes = client.recv(2048)
+    response_bytes = encode_response(decode_request(request_bytes))
+    client.sendall(response_bytes)
 
 
 def main():
@@ -11,10 +14,9 @@ def main():
     server = socket.create_server(("localhost", 9092), reuse_port=True)
 
     while True:
-        client, address = server.accept()
-        request_bytes = client.recv(2048)
-        response_bytes = encode_response(decode_request(request_bytes))
-        client.sendall(response_bytes)
+        client, _ = server.accept()
+        while True:
+            handle_client(client)
 
 
 if __name__ == "__main__":
